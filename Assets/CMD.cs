3700e629ab63e25035946e3378fd769223a6f44e
stupid999 <aierzhiguang2@gmail.com>
2019-09-06 17:12:15 +0800

    新增Cmd和Rpc 在客户端或者伺服器点击滑鼠左键皆可是数字+1

---
+++ Assets/CMD.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class CMD : NetworkBehaviour
+{
+    public int value;
+    public Text NumText;
+
+    private void Start()
+    {
+        CmdSendName("Banana");
+        NumText = GameObject.FindGameObjectWithTag("Finish").GetComponent<Text>();
+        NumText.text = "value " + value;
+    }
+
+    public void Update()
+    {
+        NumText = GameObject.FindGameObjectWithTag("Finish").GetComponent<Text>();
+        NumText.text = "value " + value;
+        if (Input.GetMouseButtonDown(0))
+        {
+            CmdAddNum(1);
+        }
+    }
+
+    [Command]
+    void CmdSendName(string name)
+    {
+        RpcUpdateName(name);
+    }
+
+    [ClientRpc]
+    void RpcUpdateName(string name)
+    {
+        transform.name = name;
+    }
+
+    [Command]
+    void CmdAddNum(int num)
+    {
+        RpcAddNum(num);
+    }
+
+    [ClientRpc]
+    void RpcAddNum(int num)
+    {
+        value += num;
+    }
+}
